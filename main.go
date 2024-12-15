c06728ecab63170a4d37e49e67012ac0bc14331c
Naruepanart Siangsanan <33704769+naruepanart@users.noreply.github.com>
2024-12-15 12:55:38 +0700

    Update main.go

--- main.go
+++ main.go
@@ -10,31 +10,31 @@ import (
 
 func main() {
 	// Get .txt files
-	f, err := filepath.Glob("*.txt")
+	fs, err := filepath.Glob("*.txt")
 	if err != nil {
 		log.Fatalf("Listing files error: %v", err)
 	}
 
 	// Guard clause for no .txt files found
-	if len(f) == 0 {
+	if len(fs) == 0 {
 		log.Println("No .txt files found.")
 		return
 	}
 
 	// Compile regex
-	r := regexp.MustCompile(`\[\d+\]`)
+	re := regexp.MustCompile(`\[\d+\]`)
 
 	// Process each file
-	for _, p := range f {
+	for _, f := range fs {
 		// Read file
-		d, err := os.ReadFile(p)
+		d, err := os.ReadFile(f)
 		if err != nil {
-			log.Printf("Failed to read file %s: %v", p, err)
+			log.Printf("Failed to read file %s: %v", f, err)
 			continue
 		}
 
 		// Clean data
-		c := r.ReplaceAll(d, nil)
+		c := re.ReplaceAll(d, nil)
 
 		// Guard clause for no changes in the file
 		if len(c) == len(d) {
@@ -42,10 +42,12 @@ func main() {
 		}
 
 		// Write cleaned data back to file
-		if err := os.WriteFile(p, c, 0644); err != nil {
-			log.Printf("Failed to write to file %s: %v", p, err)
+		if err := os.WriteFile(f, c, 0644); err != nil {
+			log.Printf("Failed to write to file %s: %v", f, err)
 			continue
 		}
-		fmt.Printf("Cleaned: %s\n", p)
+
+		fmt.Printf("Cleaned: %s\n", f)
+
 	}
 }
